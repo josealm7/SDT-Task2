1a2169033c7230fab2fdf2660ecc1c71a5e6b7e9
josealm24 <joseangealm24@gmail.com>
2026-04-22 13:15:18 +0200

    Change in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World FROM MAIN");
         
         // Initialize array with random values
         int[] table = new int[10];
